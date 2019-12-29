722eb3c32d8a208023cb0f07be07f0e9a19c4f08
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-12-30 07:09:15 +0900

    32. Creating a Functional Component

--- session3/react-complete-guide/src/App.js
+++ session3/react-complete-guide/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Person from './Person/Person';
+
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <Person />
     </div>
   );
 }
