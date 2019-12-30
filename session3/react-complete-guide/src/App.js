59b027cc0c7344983320a94a9d6a389513b28261
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-12-30 16:16:00 +0900

    72. Styled Components & Dynamic Styles

--- session3/react-complete-guide/src/App.js
+++ session3/react-complete-guide/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Person from './Person/Person';
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${props => props.altStyle ? 'red' : 'green'};
   color: white;
   font: inherit;
   border: 1px solid blue;
@@ -12,7 +12,7 @@ const StyledButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    background-color: lightgreen;
+    background-color: ${props => props.altStyle ? 'salmon' : 'lightgreen'};
     color: black;
   }
 `;
@@ -55,9 +55,6 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-    };
-
     let persons = null;
 
     if ( this.state.showPersons ) {
@@ -74,11 +71,11 @@ class App extends Component {
         </div>
       );
  
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // };
     }
 
     let classes = [];
@@ -95,6 +92,7 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         <StyledButton
+          altStyle={this.state.showPersons} // NOTE: Use altStyle intead of alt since alt is reserved.
           onClick={this.togglePersonsHandler}>Toggle Persons
         </StyledButton>
         {persons}
